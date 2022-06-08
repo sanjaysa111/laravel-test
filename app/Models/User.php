5a91658b3ddf6314133c7a2a1dd20a908564c920
Sanjay Sagathiya <sanjay@fabninjas.com>
2022-06-09 00:11:19 +0530

    Auth complete

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password', 'dob',
     ];
 
     /**
@@ -36,5 +36,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'dob' => 'datetime',
     ];
 }
